0b4bf77486bf887dd0def53a21b40e452049999f
alsino <iamahorse@gmail.com>
2022-04-03 01:25:19 +0200

    added postMessage working basic

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,25 +1,17 @@
 <script>
-	import { onMount } from 'svelte';
-
 	import { MAP_TYPE } from '$lib/stores/shared';
 	import { APP_HEIGHT } from '$lib/stores/shared';
 	import MapChoroplethAPI from '$lib/components/MapChoroplethAPI.svelte';
 	import MapChoropleth from '$lib/components/MapChoropleth.svelte';
 
 	// Send map height to parent window
-	// $: {
-	// 	if ($APP_HEIGHT) {
-	// 		// window.parent.postMessage({ height: $APP_HEIGHT }, 'http://localhost:3000/');
-	// 		window.parent.postMessage({ height: $APP_HEIGHT }, 'https://europe-map.vercel.app/');
-	// 		console.log('message sent', $APP_HEIGHT);
-	// 	}
-	// }
-
-	onMount(() => {
-		console.log('app ready');
-		window.parent.postMessage('this is a test', 'https://europe-map.vercel.app/');
-		// window.parent.postMessage({ height: 135 }, 'https://europe-map.vercel.app/');
-	});
+	$: {
+		if ($APP_HEIGHT) {
+			window.parent.postMessage({ height: $APP_HEIGHT }, 'https://europe-map.vercel.app/');
+			// window.parent.postMessage({ height: $APP_HEIGHT }, 'http://localhost:3000/');
+			// window.parent.postMessage({ height: $APP_HEIGHT }, 'https://europe-map.vercel.app/');
+		}
+	}
 </script>
 
 <div id="euranet-map" bind:clientHeight={$APP_HEIGHT}>
