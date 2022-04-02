886e1562b4fd86d1984afc21da4b9a3515983dbb
alsino <iamahorse@gmail.com>
2022-04-03 01:41:08 +0200

    added postMessage working 4

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -7,7 +7,7 @@
 	// Send map height to parent window
 	$: {
 		if ($APP_HEIGHT) {
-			window.parent.postMessage({ height: $APP_HEIGHT }, 'https://europe-map.vercel.app/');
+			window.parent.postMessage({ height: $APP_HEIGHT }, 'http://127.0.0.1:8080/');
 			// window.parent.postMessage({ height: $APP_HEIGHT }, 'http://localhost:3000/');
 			// window.parent.postMessage({ height: $APP_HEIGHT }, 'https://europe-map.vercel.app/');
 		}
