a617750f4d397f50407ddf59f6156457e082395c
alsino <iamahorse@gmail.com>
2022-04-10 10:40:38 +0200

    better script highlighting translate js

--- translate.js
+++ translate.js
@@ -4,7 +4,9 @@ import dotenv from 'dotenv'
 import * as fs from 'fs';
 dotenv.config()
 
-console.log(chalk.green('Getting translations from Google API...'));
+// All language codes: https://cloud.google.com/translate/docs/languages
+
+console.log(chalk.green('Starting translation process...'));
 
 // Import data
 const require = createRequire(import.meta.url);
@@ -17,9 +19,14 @@ const { Translate } = require('@google-cloud/translate').v2;
 // Instantiates a client
 const client = new Translate({ key: process.env.API_KEY });
 
+console.log(chalk.yellow(`Getting translations from Google API for the following ${languages.languages.length} languages:`));
+languages.languages.forEach((lang,i) => {
+  console.log(chalk.yellow(i + 1, lang.label))
+})
+
 
 // Translate all contents of en.json file
-async function translateJSON(target) {  
+async function translateJSON(target, label) {  
   var desktopTranslated = {};
 
    for (var key in sourceJSON) {
@@ -31,27 +38,27 @@ async function translateJSON(target) {
     });
    }
   
-  console.log(target, desktopTranslated);
-  writeJSONToFile(desktopTranslated, target);
+  // console.log(target, desktopTranslated);
+  writeJSONToFile(desktopTranslated, target, label);
 }
 
 // Translate for all available languages
 languages.languages.forEach((item) => {
-  translateJSON(item.value)
+  translateJSON(item.value, item.label)
 })
 
 
 // Write translation result to JSON file
-function writeJSONToFile(jsonObj, target) {
+function writeJSONToFile(jsonObj, target, label) {
   // convert JSON object to string
   const data = JSON.stringify(jsonObj, null, 4);
 
   // write JSON string to a file
   fs.writeFile(`./static/languages/${target}.json`, data, (err) => {
     if (err) {
-      throw err;
+      console.log(chalk.red("Something went wrong!", err))
     }
-    console.log("JSON data is saved.");
+    console.log(chalk.green(`Success! Translation file saved for: ${label} -> ${target}.json `))
   });
 }
 
